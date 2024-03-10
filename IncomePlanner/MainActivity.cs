452d6a9dd61585f5045d3e20ed84ccc399d8cd34
Timz <timyvillarmia@gmail.com>
2024-03-11 00:14:04 +0800

    feat: DropDownMenu

--- IncomePlanner/MainActivity.cs
+++ IncomePlanner/MainActivity.cs
@@ -1,8 +1,11 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Runtime;
+using Android.Views;
+using Android.Widget;
 using AndroidX.AppCompat.App;
 using Google.Android.Material.TextField;
+using System;
 
 namespace IncomePlanner
 {
@@ -18,7 +21,15 @@ namespace IncomePlanner
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
 
+            AutoCompleteTextView dropdown_menu = FindViewById<AutoCompleteTextView>(Resource.Id.dropdown_item);
 
+
+            String[] COUNTRIES = new String[] { "Item 1", "Item 2", "Item 3", "Item 4" };
+
+            var adapter = ArrayAdapter.CreateFromResource(
+                    this, Resource.Array.period_array, Android.Resource.Layout.SimpleSpinnerDropDownItem);
+
+            dropdown_menu.Adapter = adapter;
 
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
